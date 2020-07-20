0e5a7c546ce7d74bea218e45b27a27117ed5ecae
potato <riyadh.razzaq@gmail.com>
2020-07-20 14:28:02 +0600

    folderwise section added

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-PROJECT_PROPERTIES = ['title','description','date','image','more',"tools"]
+"""config files for user."""
+CONTENT_PATH = 'content/'
+# SECTIONS = ['Works & Other Stuff', 'Publications']
+# PROJECT_PROPERTIES = ['title', 'description', 'date', 'image', 'more', "tools"]
+# PUBLICATION_PROPERTIES = ['title', 'description',
+#                           'date', 'more', 'tools', 'reference']
+SECTIONS = [{
+    "heading": "Works & Other Stuff",
+    "properties" : ['title', 'description', 'date', 'image', 'more', "tools"]
+},
+    {
+    "heading": "Publications",
+    "properties": ['title', 'description', 'date', 'more', 'tools', 'reference']
+}
+]
